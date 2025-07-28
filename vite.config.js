6d02c30cbc96fb1b540d6b70afaf757a07b59cb0
Diego Sucharczuk <diego.sucharczuk@cyberark.com>
2025-07-28 08:55:57 +0300

    tt

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Yahav/' : '/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
